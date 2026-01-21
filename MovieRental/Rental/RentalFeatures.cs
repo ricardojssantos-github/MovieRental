5e08b61b0f4ff239246de968303f9e0a2df448df
Ricardo Santos <Ricardo Santos@DESKTOP-SPE5JQB>
2026-01-21 02:08:24 +0000

    Made the method Save async

--- MovieRental/Rental/RentalFeatures.cs
+++ MovieRental/Rental/RentalFeatures.cs
@@ -12,10 +12,10 @@ namespace MovieRental.Rental
 		}
 
 		//TODO: make me async :(
-		public Rental Save(Rental rental)
+		public async Task<Rental> SaveAsync(Rental rental)
 		{
-			_movieRentalDb.Rentals.Add(rental);
-			_movieRentalDb.SaveChanges();
+			await _movieRentalDb.Rentals.AddAsync(rental);
+			await _movieRentalDb.SaveChangesAsync();
 			return rental;
 		}
 
