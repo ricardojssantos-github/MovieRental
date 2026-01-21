e2487424fc73cbb46aa0db6bebfc3d41bae17c0b
Ricardo Santos <Ricardo Santos@DESKTOP-SPE5JQB>
2026-01-21 02:12:12 +0000

    Updated interface for SaveAsync

--- MovieRental/Rental/IRentalFeatures.cs
+++ MovieRental/Rental/IRentalFeatures.cs
@@ -2,6 +2,6 @@
 
 public interface IRentalFeatures
 {
-	Rental Save(Rental rental);
-	IEnumerable<Rental> GetRentalsByCustomerName(string customerName);
+    Task<Rental> SaveAsync(Rental rental);
+    IEnumerable<Rental> GetRentalsByCustomerName(string customerName);
 }
